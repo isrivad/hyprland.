f94af069ed9f8e5567a26707cf1dfb2fea447eb6
isrivad <isrivad@gmail.com>
2025-09-10 10:00:21 -0500

    Create scriptures.sh

---
+++ config/hypr/scripts/alttab/scriptures.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+verses=(
+"Psalm 23:1 - The Lord is my shepherd; I shall not want."
+"Philippians 4:13 - I can do all things through Christ who strengthens me."
+"Romans 8:28 - All things work together for good to those who love God."
+"John 3:16 - For God so loved the world, that he gave his only Son."
+)
